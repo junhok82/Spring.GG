984d79635b7dd7c824b649af296511676dee2321
junhok82 <junhok82@gmail.com>
2020-06-03 18:23:21 +0900

    preparation : 소환사 정보 파싱하기
    1. DELETE
       -
    
    2. CREATE
       - Controller.java : 소환사 정보를 이름으로 GET
       - Summoner.java : 소환사 정보 DTO
       - SummonerParser.java : json 형식으로 소환사 정보를 얻는 파서
    
    3.UPDATE
       - pom.xml : json 파싱을 위한 httpclient 라이브러리 추가

--- SpringGG/src/main/java/com/junho/controller/Controller.java
+++ SpringGG/src/main/java/com/junho/controller/Controller.java
@@ -1,5 +1,27 @@
 package com.junho.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.junho.dto.Summoner;
+import com.junho.util.SummonerParser;
+
+@RestController
+@CrossOrigin(origins= {"*"}, maxAge=6000)
 public class Controller {
 
+	@GetMapping(value = "/summoner/{name}")
+	public ResponseEntity<Summoner> summoner(@PathVariable String name) {
+		SummonerParser summonerParser = new SummonerParser();
+		Summoner summoner = summonerParser.getJsonData(name);
+
+		if(summoner != null && !summoner.equals(null)) {
+			return new ResponseEntity<Summoner>(summoner,HttpStatus.OK);
+		}
+		return new ResponseEntity<Summoner>(HttpStatus.NO_CONTENT);
+	}
 }
---
+++ SpringGG/src/main/java/com/junho/util/SummonerParser.java
@@ -0,0 +1,48 @@
+package com.junho.util;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.junho.dto.Summoner;
+
+
+
+public class SummonerParser {
+	
+	public Summoner getJsonData(String name) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		// 공백 처리
+		String SummonerName = name.replaceAll(" ", "%20");
+		System.out.println(SummonerName);
+		
+		String requestURL 	= "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ SummonerName + "?api_key=" + Key.API_KEY;
+		Summoner summoner 	= null;
+		
+		try {
+			HttpClient client = HttpClientBuilder.create().build(); // HttpClient 생성
+			HttpGet getRequest = new HttpGet(requestURL); //GET 메소드 URL 생성
+			HttpResponse response = client.execute(getRequest);
+	
+			//Response 출력
+			if (response.getStatusLine().getStatusCode() == 200) {
+				ResponseHandler<String> handler = new BasicResponseHandler();
+				String body = handler.handleResponse(response);
+				
+				summoner = objectMapper.readValue(body, Summoner.class);   // String to Object로 변환
+				 		
+			} else {
+				System.out.println("response is error : " + response.getStatusLine().getStatusCode());
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return summoner;
+	}
+}
